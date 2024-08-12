d14a0f36ad6bc702a22283aa530ca1ccdfa48feb
zYuna646 <anggarangga646@gmail.com>
2024-08-13 06:05:47 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [FrontPageController::class, 'index'])->name('home');
+Route::get('/', 'Auth\LoginController@showLoginForm')->name('home');
 Route::get('/catalog', [FrontPageController::class, 'catalog'])->name('catalog');
 Route::get('/catalog/product/{slug}', [FrontPageController::class, 'productDetail'])->name('product.detail');
 

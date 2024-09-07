2eba09e337697589a7975f5d19566a8a5d151075
zYuna646 <anggarangga646@gmail.com>
2024-09-07 20:43:26 +0800

    fix

--- database/migrations/2019_09_05_050808_create_periodes_table.php
+++ database/migrations/2019_09_05_050808_create_periodes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('periodes', function (Blueprint $table) {
             $table->id();
             $table->string('tahun');
+            
             $table->timestamps();
         });
     }
